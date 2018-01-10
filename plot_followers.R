2bc45957ade1d34abb96fb1e64b0328844c8c0da
Adrian Logue <adrian@cloverfield.com.au>
2018-01-10 13:48:54 +1100

    Minor tidy up of followers charting code

--- plot_followers.R
+++ plot_followers.R
@@ -1,10 +1,10 @@
 # followers and follow_ratio analysis
-data_accounts_working <- data_accounts
+data_orderby_followers <- data_accounts_working
 
-data_accounts_working$screen_name <- reorder(data_accounts_working$screen_name, data_accounts_working$followers_count)
+data_orderby_followers$screen_name <- reorder(data_orderby_followers$screen_name, data_orderby_followers$followers_count)
 
 # order this, then select top 50
-data_orderby_followers <- data_accounts_working[with(data_accounts_working, order(-followers_count)), ]
+data_orderby_followers <- data_orderby_followers[with(data_orderby_followers, order(-followers_count)), ]
 
 forPlot <- data_orderby_followers[1:50, ]
 
@@ -21,12 +21,12 @@ p + theme_cowplot(font_family = "Avenir Next") +
 # TODO: Work out how to set the friends_count to 1 where it is zero to avoid divide by zero
 
 # I also want to look at the follow ratio
-data_accounts_working$follow_ratio <- data_accounts_working$followers_count / data_accounts_working$friends_count
+data_orderby_followers$follow_ratio <- data_orderby_followers$followers_count / data_orderby_followers$friends_count
 
-data_accounts_working$screen_name <- reorder(data_accounts_working$screen_name, data_accounts_working$follow_ratio)
+data_orderby_followers$screen_name <- reorder(data_orderby_followers$screen_name, data_orderby_followers$follow_ratio)
 
 # order this, then select top 50
-data_orderby_followers <- data_accounts_working[with(data_accounts_working, order(-follow_ratio)), ]
+data_orderby_followers <- data_orderby_followers[with(data_orderby_followers, order(-follow_ratio)), ]
 
 forPlot <- data_orderby_followers[1:50, ]
 
